cd9b10e574345342001636159cb3ca0644f60258
Robert Liu <robert.liu@graidtech.com>
2022-11-21 17:51:03 +0800

    busy_loop: Support options

--- cuda/busy_loop/busy_loop.cu
+++ cuda/busy_loop/busy_loop.cu
@@ -3,9 +3,12 @@
 #include <math.h>
 #include <chrono>
 #include <ctime>
+#include <unistd.h>
 
+#define SMEM_SZ (48 << 10)
 #define BLOCK_NR 14
 #define BLOCK_SZ 1024
+#define LOOP_SEC 1
 
 __device__
 void clock_block(clock_t clock_count)
@@ -23,25 +26,22 @@ void sleep_ms(uint64_t ms)
 		__nanosleep(ms);
 }
 
-#define SMEM_SZ (24 << 10)
 __global__
 __launch_bounds__(BLOCK_SZ, 1)
-void busy_loop(int endless)
+void busy_loop(int time)
 {
-	volatile __shared__ uint8_t s[SMEM_SZ << 1];
-	const bool grid_leader = (blockIdx.x == 0 && threadIdx.x == 0);
+	volatile __shared__ uint8_t s[SMEM_SZ];
+	const uint32_t half_smem_sz = SMEM_SZ >> 1;
 	const bool block_leader = (threadIdx.x == 0);
 
 	if (block_leader)
-		printf("Block %d\n", blockIdx.x);
-	if (grid_leader && endless)
-		printf("Endless\n");
+		printf("Activate B[%d]\n", blockIdx.x);
 
-	if (endless) {
+	if (!time) {
 		while(1) {
-			for (int i = 0; i < (SMEM_SZ / BLOCK_SZ); i += BLOCK_SZ) {
-				if (threadIdx.x + i < SMEM_SZ)
-					s[threadIdx.x + i + SMEM_SZ] = s[threadIdx.x + i];
+			for (int i = 0; i < (half_smem_sz / gridDim.x); i += gridDim.x) {
+				if (threadIdx.x + i < half_smem_sz)
+					s[threadIdx.x + i + half_smem_sz] = s[threadIdx.x + i];
 			}
 			//clock_block(2^63);
 			sleep_ms(1000);
@@ -51,26 +51,83 @@ void busy_loop(int endless)
 		}
 	} else {
 		//clock_block(2^63);
-		sleep_ms(1000);
+		sleep_ms(1000 * time);
 		__syncthreads();
 	}
 }
 
+void usage()
+{
+	printf("Usage: busy_loop [OPTION]...\n");
+	printf("\n");
+	printf("  -d, GPU device ID, default = 0\n");
+	printf("  -n, GPU kernel block number, default = %d\n", BLOCK_NR);
+	printf("  -s, GPU kernel block size, default = %d\n", BLOCK_SZ);
+	printf("  -t, Loop time in second, default = %d, set 0 for endless\n", LOOP_SEC);
+	printf("  -h, Disaply usage\n");
+}
+
 int main(int argc, char *argv[])
 {
-	int endless = 0;
-	char *c;
+	int dev_id = 0;
+	int block_nr = BLOCK_NR;
+	int block_sz = BLOCK_SZ;
+	int time = LOOP_SEC;
 
-	if (argc >= 2) {
-		endless = strtol(argv[1], &c, 10);
+	int o;
+	const char *optstring = "d:n:s:t:h";
+	while ((o = getopt(argc, argv, optstring)) != -1) {
+		switch (o) {
+			case 'd':
+				dev_id = std::stoi(optarg);
+				break;
+			case 'n':
+				block_nr = std::stoi(optarg);
+				break;
+			case 's':
+				block_sz = std::stoi(optarg);
+				break;
+			case 't':
+				time = std::stoi(optarg);
+				break;
+			case 'h':
+				usage();
+				return 0;
+			default:
+				printf("Unknown args %s\n", optarg);
+				usage();
+				return -1;
+		}
 	}
 
-	// Set desired GPU device ID
-	cudaSetDevice(0);
+	cudaError_t rc;
+
+	rc = cudaSetDevice(dev_id);
+	if (rc != cudaSuccess) {
+		printf("Failed to set GPU device %d, %s\n", dev_id, cudaGetErrorString(rc));
+		return -1;
+	}
+
+	if (!time) {
+		printf("Endless mode\n");
+	} else {
+		printf("Loop for %d second\n", time);
+	}
 
 	auto start = std::chrono::system_clock::now();
-	busy_loop<<<BLOCK_NR, BLOCK_SZ>>>(endless);
-	cudaDeviceSynchronize();
+	busy_loop<<<block_nr, block_sz>>>(time);
+	rc = cudaGetLastError();
+	if (rc != cudaSuccess) {
+		printf("Failed to launch kernel on device %d, %s\n", dev_id, cudaGetErrorString(rc));
+		return -1;
+	}
+
+	rc = cudaDeviceSynchronize();
+	if (rc != cudaSuccess) {
+		printf("Failed to sync device %d, %s\n", dev_id, cudaGetErrorString(rc));
+		return -1;
+	}
+
 	auto end = std::chrono::system_clock::now();
 
 	std::chrono::duration<double> elapsed_seconds = end - start;
